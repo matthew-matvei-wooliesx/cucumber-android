0397be29ed8a485f2982707c10ee55b00b4cab34
Matthew James <mjames4@woolworths.com.au>
2022-01-24 05:15:07 +1100

    Implement counter expectations

--- app/src/androidTest/assets/features/counter.feature
+++ app/src/androidTest/assets/features/counter.feature
@@ -8,3 +8,33 @@ Feature: Counter
     Examples:
       | count |
       | 0 |
+
+  Scenario Outline: The counter can be incremented
+    Given the counter starts at <count>
+    When the user increments the counter <x> times
+    Then the counter should display "<result>"
+
+    Examples:
+      | count | x | result |
+      | 5 | 5 | 10 |
+      | 2 | 3 | 5 |
+
+  Scenario Outline: The counter can be decremented
+    Given the counter starts at <count>
+    When the user decrements the counter <x> times
+    Then the counter should display "<result>"
+
+    Examples:
+      | count | x | result |
+      | 5 | 5 | 0 |
+      | 3 | 2 | 1 |
+
+  Scenario Outline: The counter cannot be decremented past '0'
+    Given the counter starts at <count>
+    When the user decrements the counter <x> times
+    Then the counter should display "<result>"
+
+    Examples:
+      | count | x | result |
+      | 3 | 5 | 0 |
+      | 1 | 15 | 0 |
--- app/src/androidTest/java/com/example/cucumberandroid/test/steps/CounterSteps.kt
+++ app/src/androidTest/java/com/example/cucumberandroid/test/steps/CounterSteps.kt
@@ -2,22 +2,66 @@ package com.example.cucumberandroid.test.steps
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.cucumberandroid.MainActivity
 import com.example.cucumberandroid.R
+import io.cucumber.java.Before
+import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
 
 class CounterSteps {
+    @Before
+    fun setup() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
     @When("the application starts")
     fun applicationStarts() {
-        ActivityScenario.launch(MainActivity::class.java)
+        // Application already started in @Before hook
     }
 
     @Then("the counter should display {string}")
     fun counterShouldDisplay(count: String) {
-        Espresso.onView(ViewMatchers.withId(R.id.counter))
-            .check(ViewAssertions.matches(ViewMatchers.withText(count)))
+        onView(withId(R.id.counter)).check(matches(withText(count)))
+    }
+
+    @Given("the counter starts at {int}")
+    fun counterStartsAt(count: Int) {
+        incrementCounterXTimes(count)
+
+        onView(withId(R.id.incrementer)).check(matches(withText(count)))
+    }
+
+    @When("the user increments the counter {int} times")
+    fun userIncrementsCounterXTimes(x: Int) {
+        incrementCounterXTimes(x)
+    }
+
+    @When("the user decrements the counter {int} times")
+    fun userDecrementsCounterXTimes(x: Int) {
+        decrementCounterXTimes(x)
+    }
+
+    private fun incrementCounterXTimes(x: Int) {
+        val incrementer = onView(withId(R.id.incrementer))
+
+        for (i in 1..x) {
+            incrementer.perform(click())
+        }
+    }
+
+    private fun decrementCounterXTimes(x: Int) {
+        val decrementer = onView(withId(R.id.decrementer))
+
+        for (i in 1..x) {
+            decrementer.perform(click())
+        }
     }
 }
